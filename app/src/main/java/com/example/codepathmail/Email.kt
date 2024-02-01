7dfb78edb55393dea4ead592662fdca6f25007f7
bab8 <bab8@njit.edu>
2024-02-01 15:27:03 -0500

    CodePathMail

---
+++ app/src/main/java/com/example/codepathmail/Email.kt
@@ -0,0 +1,12 @@
+package com.example.codepathmail
+
+import android.widget.ImageView
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String,
+    val date: String,
+    val imageView: Int)  {
+
+}
